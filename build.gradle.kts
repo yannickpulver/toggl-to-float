ea0a4c48370ec5bc60de2b31ffa1b101753e4e38
Yannick <yannick@appswithlove.com>
2024-02-05 14:06:06 +0100

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.appswithlove"
-version = "1.3.7"
+version = "1.4.0"
 
 repositories {
     google()
@@ -53,7 +53,7 @@ compose.desktop {
             modules("jdk.unsupported")
             targetFormats(TargetFormat.Dmg, TargetFormat.Exe)
             packageName = "Toggl2Float"
-            packageVersion = "1.3.7"
+            packageVersion = "1.4.0"
             macOS {
                 iconFile.set(project.file("icon.icns"))
                 bundleID = "com.appswithlove.toggl2float"
