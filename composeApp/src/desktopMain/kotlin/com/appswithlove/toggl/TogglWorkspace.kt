5497ed83c8216f54161be7a63f1dad9984c229e1
Yannick <yannick@appswithlove.com>
2024-12-17 13:55:12 +0100

    Implements API changes from Toggl

--- composeApp/src/desktopMain/kotlin/com/appswithlove/toggl/TogglWorkspace.kt
+++ composeApp/src/desktopMain/kotlin/com/appswithlove/toggl/TogglWorkspace.kt
@@ -22,7 +22,6 @@ data class TogglWorkspaceItem(
     val only_admins_see_team_dashboard: Boolean,
     val organization_id: Int,
     val premium: Boolean,
-    val profile: Int,
     val projects_billable_by_default: Boolean,
     val rate_last_updated: String? = null,
     val reports_collapse: Boolean,
